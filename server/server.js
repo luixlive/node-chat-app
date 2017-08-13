ddf8a5ca4f3276ccd7dc1e6a0bfac2efcec9bcf2
Luis Alfonso <luis.chavez@wizeline.com>
2017-08-13 14:03:03 -0500

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -15,6 +15,18 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the Chat App',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  });
+
   socket.on('createMessage', ({from, text}) => {
     console.log('createMessage', {from, text});
     io.emit('newMessage', {
