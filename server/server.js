134c90bb34f8c0c688ef51f505b87f49de604719
Luis Alfonso <luis.chavez@wizeline.com>
2017-08-13 13:12:41 -0500

    Add new message and create message events

--- server/server.js
+++ server/server.js
@@ -15,6 +15,16 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'luixlive',
+    message: 'Hey',
+    createdAt: 123
+  });
+
+  socket.on('createMessage', (newMessage) => {
+    console.log('createMessage', newMessage);
+  });
+
   socket.on('disconnect', () => {
     console.log('User was disconnected');
   });
